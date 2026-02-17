ac61840567ad61c98c79748e6f6621e17fb5adb5
Ganymede <jirawatkdm@gmail.com>
2026-02-17 14:08:59 +0700

    fix url

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -501,7 +501,7 @@ export default function MainApp() {
         )}
       </div>
       <a
-        href="http://localhost/hii-kiosk/#/staff_set_other"
+        href="http://localhost/hii-kiosk/#/other_services"
         className="fixed bottom-8 left-8 flex items-center gap-2 px-6 py-3 bg-white text-indigo-900 font-bold rounded-full shadow-lg hover:bg-indigo-50 transition-all border border-indigo-100 active:scale-95"
       >
         <ArrowLeft size={20} />
